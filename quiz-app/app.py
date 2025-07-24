8f708f40e22d2d8b9ef477ca677334631c4ff2ea
Gampalavinuthna <vinuthnagampala@gmail.com>
2025-07-24 06:56:01 +0530

    Update app.py

--- quiz-app/app.py
+++ quiz-app/app.py
@@ -1,117 +1,17 @@
-from flask import Flask, request, jsonify, render_template
-
-import boto3
-
+from flask import Flask, jsonify, render_template
 import json
 
-from pymongo import MongoClient
-
-from flask_cors import CORS
-
-
-
-app = Flask(_name_)
-
-CORS(app)
-
-
-
-# Connect to MongoDB Atlas
-
-client = MongoClient("your-mongodb-connection-string")
-
-db = client["quizdb"]
-
-results_collection = db["results"]
-
-
-
-# S3
-
-s3 = boto3.client('s3',
-
-    aws_access_key_id="YOUR_AWS_ACCESS_KEY",
-
-    aws_secret_access_key="YOUR_SECRET_KEY"
-
-)
-
-bucket_name = "your-s3-bucket-name"
-
-
+app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html')
 
-def home():
-
-    return render_template("index.html")
-
-
-
-@app.route('/questions', methods=['GET'])
-
+@app.route('/questions')
 def get_questions():
+    with open('questions.json') as f:
+        data = json.load(f)
+    return jsonify(data)
 
-    obj = s3.get_object(Bucket=bucket_name, Key='questions.json')
-
-    questions = json.loads(obj['Body'].read())
-
-    return jsonify(questions)
-
-
-
-@app.route('/submit', methods=['POST'])
-
-def submit_answers():
-
-    data = request.json
-
-    results_collection.insert_one(data)
-
-    return jsonify({"message": "Results saved!"})
-
-
-
-if _name_ == '_main_':
-
-    app.run(host='0.0.0.0', port=80)
-
-from flask import Flask, request, jsonify, render_template
-import boto3
-import json
-from pymongo import MongoClient
-from flask_cors import CORS
-
-app = Flask(_name_)
-CORS(app)
-
-# Connect to MongoDB Atlas
-client = MongoClient("your-mongodb-connection-string")
-db = client["quizdb"]
-results_collection = db["results"]
-
-# S3
-s3 = boto3.client('s3',
-    aws_access_key_id="YOUR_AWS_ACCESS_KEY",
-    aws_secret_access_key="YOUR_SECRET_KEY"
-)
-bucket_name = "your-s3-bucket-name"
-
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-@app.route('/questions', methods=['GET'])
-def get_questions():
-    obj = s3.get_object(Bucket=bucket_name, Key='questions.json')
-    questions = json.loads(obj['Body'].read())
-    return jsonify(questions)
-
-@app.route('/submit', methods=['POST'])
-def submit_answers():
-    data = request.json
-    results_collection.insert_one(data)
-    return jsonify({"message": "Results saved!"})
-
-if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
